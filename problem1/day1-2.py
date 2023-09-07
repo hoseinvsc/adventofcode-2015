bd39dbf9b72f0e787a73a007ee53bd6e92c8d3cd
hoseinvsc <hoseinvsc@gmail.com>
2023-09-07 16:28:40 +0330

    H

--- problem1/day1-2.py
+++ problem1/day1-2.py
@@ -1,20 +1,6 @@
+from day1_module import balance_founder
+
 file = open("problem1/day1.txt","r")
 read_file= file.read()
 
-balance = 0
-counter = 0
-for char in read_file:
-    counter = counter+1
-    print(char,counter,balance)
-
-    if char == "(":
-        balance = balance + 1
-    elif char == ")":
-        balance = balance - 1
-
-    if balance == -1:
-        print("*************")
-        break
-
-print(balance)
-print(counter)
+print(balance_founder(read_file))
--- problem1/day1_module.py
+++ problem1/day1_module.py
@@ -1,3 +1,4 @@
+#This is for part1
 def check_balance(file):
     balance = 0
     for char in file:
@@ -6,3 +7,21 @@ def check_balance(file):
         elif char == ")":
             balance = balance - 1
     return balance
+
+
+#This is for part2
+def balance_founder(file2):
+    balance2 = 0
+    counter = 0
+    for char in file2:
+        counter = counter+1
+        print(char,counter,balance2)
+
+        if char == "(":
+            balance2 = balance2 + 1
+        elif char == ")":
+            balance2 = balance2 - 1
+
+        if balance2 == -1:
+            print("****************")
+            break
