e47035e264825c68e63efc6722517ee8cdd6199e
hoseinvsc <hoseinvsc@gmail.com>
2023-09-07 15:25:26 +0330

    new module for day1.

--- problem1/day1-1.py
+++ problem1/day1-1.py
@@ -1,13 +1,15 @@
 # https://adventofcode.com/2015/day/1
-# my input must be read from input.txt 
-file = open("problem1/d.txt","r")
+
+file = open("problem1/day1.txt","r")
 read_file= file.read()
 
-balance = 0
-for char in read_file:
-    if char == "(":
-        balance = balance + 1
-    elif char == ")":
-        balance = balance - 1
+def check_balance(read_file):
+    balance = 0
+    for char in read_file:
+        if char == "(":
+            balance = balance + 1
+        elif char == ")":
+            balance = balance - 1
+    return balance
 
-print(balance)
+print(check_balance(read_file))
--- problem1/day1_module.py
+++ problem1/day1_module.py
@@ -1,2 +1,12 @@
-# def balance_calculator(txt):
-    # return balance 
+def check_balance(file):
+    balance = 0
+    for char in file:
+        if char == "(":
+            balance = balance + 1
+        elif char == ")":
+            balance = balance - 1
+    return balance
+
+
+file = "(()"
+print(check_balance(file))
